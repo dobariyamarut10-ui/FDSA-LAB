1e82c36cdc80abda431f035dda80dc603b2c98fe
Marut Dobariya <dobariyamarut10@gmail.com>
2026-08-13 16:11:48 +0530

    Update Prac2-2-1.cpp

--- Practical 2/Prac2-2-1.cpp
+++ Practical 2/Prac2-2-1.cpp
@@ -1,50 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int findBook(int arr[],int bookCode,int left,int right)
+int findBook(int arr[], int bookCode, int left, int right)
 {
-    int mid=left+(right-left)/2;
+   if (left > right)
+   {
+      return -1;
+   }
 
-         if(left>=right)
-         {
-            return -1;
-         } 
-         else if(arr[mid]==bookCode)
-         {
-            return mid+1;
-         } 
-         else if(arr[mid]<bookCode)
-         {
-            return findBook( arr, bookCode, mid+1, right);
-         }  
-         else 
-         {
-           return findBook( arr, bookCode, right, mid-1);
-         }  
+   int mid = left + (right - left) / 2;
 
-         return -1;
+   if (arr[mid] == bookCode)
+   {
+      return mid + 1;
+   }
+   else if (arr[mid] < bookCode)
+   {
+      return findBook(arr, bookCode, mid + 1, right);
+   }
+   else
+   {
+      return findBook(arr, bookCode, left, mid - 1);
+   }
 }
+
 int main()
 {
-    int n;
-    cout<<"How many book code you want to enter"<<endl;
-    cin>>n;
+   int n;
+   cout << "How many book codes you want to enter" << endl;
+   cin >> n;
 
-    int position=0;
-    int left=0;
-    int right=n-1;
-    int bookCode;
-    int arr[n];
+   int left = 0;
+   int right = n - 1;
+   int bookCode;
+   int arr[n];
 
+   for (int i = 0; i < n; i++)
+   {
+      cout << "Enter book code" << endl;
+      cin >> arr[i];
+   }
 
-    for(int i=0;i<n;i++)
-    {
-       cout<<"Enter book code"<<endl;
-       cin>>arr[i];
-    }
-    cout<<"Enter book code that you want to search"<<endl;
-    cin>>bookCode;
+   cout << "Enter book code that you want to search" << endl;
+   cin >> bookCode;
 
-    cout<<"Your book found at position "<<findBook(arr,bookCode,left,right)<<endl;
-    
+   cout << "Your book found at position "
+        << findBook(arr, bookCode, left, right) << endl;
+
+   return 0;
 }
